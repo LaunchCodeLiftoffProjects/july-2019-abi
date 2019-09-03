31a4c1c43da0892ac2e59d05943beb766c294d3e
Tracy Schneider <trac61@hotmail.com>
2019-09-03 16:19:02 -0500

    changing team class to roster and refactoring

--- src/test/java/org/launchcode/liftoff/controllers/RosterController.java
+++ src/test/java/org/launchcode/liftoff/controllers/RosterController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("roster")
+@RequestMapping("/roster")
 public class RosterController {
     @Autowired
     public RosterDao rosterDao;
